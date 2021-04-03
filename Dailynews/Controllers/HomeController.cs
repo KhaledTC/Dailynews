30e9a301f77f769ecfe64cf443b10dbad80d5f3e
Khaled Muhammad <khaldmohamed.cf.tc.cpp@gmail.com>
2021-04-03 18:26:43 +0200

    first commit

---
+++ Dailynews/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Dailynews.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Dailynews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<News> model = new List<News>();
+            for (int i = 1; i <= 3; i++)
+            {
+                string agency = "ag" + i.ToString();
+                string JsonFileName = agency + ".json";
+                StreamReader r = new StreamReader("Jsons/" + JsonFileName);
+                string jsonContent = r.ReadToEnd();
+                News news = JsonConvert.DeserializeObject<News>(jsonContent);
+                foreach (var n in news.latestnews)
+                    n.source = agency;
+                model.Add(news);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Dailynews/Models/New.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dailynews.Models
+{
+    public class New
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+        public string rating { get; set; }
+        public string datetime { get; set; }
+        public string source { get; set; }
+    }
+}
